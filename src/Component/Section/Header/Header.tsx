5b2588808630b01162bc1160303e74595077e1fc
Yuric888 <111116745+Yuric888@users.noreply.github.com>
2023-02-07 07:48:21 +0700

    change header

--- src/Component/Section/Header/Header.tsx
+++ src/Component/Section/Header/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
       }`}
       style={state}
     >
-      <div className="container d-flex justify-content-between align-items-center py-2 px-2">
+      <div className="container d-flex justify-content-between align-items-center py-2 px-5">
         <Logo />
         <NavLinkHeader />
         <Themes />
