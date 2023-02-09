211d945ec150c2f145b9fb93974185d20a465535
Yuric888 <111116745+Yuric888@users.noreply.github.com>
2023-02-09 09:47:30 +0700

    fix-navlink-logo

--- src/Component/Logo/Logo.tsx
+++ src/Component/Logo/Logo.tsx
@@ -3,8 +3,14 @@ import Image from "../../assets/svg/logo-portfolio.svg";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
-    <Link to="/" className="logo">
+    <Link to="/" className="logo" onClick={scrollToTop}>
       <img src={Image} alt="logo" />
     </Link>
   );
